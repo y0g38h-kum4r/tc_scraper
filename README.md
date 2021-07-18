94d37c328fddfae0448e4a97315daa54d5b5bc67
yogesh kumar <yogeshkumar.m200010@gmail.com>
2021-07-19 02:26:50 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Python script to analyse Competitive Programming matches from site https://www.topcoder.com/ using BeautifulSoup4.
+
+Features available: Problem Ratings (feature not availabe in topcoder), Links to problems, Problemwise analysis!!
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+      with open('README.md') as f:
+            return f.read()
+            
+setup(name='tc_scraper',
+      version='0.1',
+      description='Software Project',
+      long_description = readme(),
+      long_description_content_type='text/markdown', 
+      
+      classifiers=[ 'License :: OSI Approved :: MIT License', 
+      'Programming Language :: Python :: 3', 
+      'Operating System :: OS Independent' ],
+      
+      url='https://github.com/y0g38h-kum4r/Topcoder_scraper',
+      author='y0g38h-kum4r',
+      author_email='yogeshkumar.m200010@gmail.com',
+      keywords = 'Software',
+      packages = ['tc_scraper'],
+      install_requires = [],
+      include_package_data = True,
+      zip_safe=False)
